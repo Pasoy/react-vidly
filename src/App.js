0a24ff80070d657d9a5350f90292a64668b9bf97
Pasoy <43441885+Pasoy@users.noreply.github.com>
2019-01-16 18:58:42 +0100

    Routing, navbar...
    - Installed React Router DOM
    - Added routes
    - Added a navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
 import './App.css';
-import './components/movies';
+
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/404';
+import NavBar from './components/partials/navbar';
 
 class App extends Component {
     render() {
         return (
-            <main className="container">
-                <h1>Yaaaw that's hawt</h1>
-                <Movies />
-            </main>
+            <React.Fragment>
+                <NavBar />
+                <main className="container">
+                    <Switch>
+                        <Route path="/movies" component={Movies} />
+                        <Route path="/customers" component={Customers} />
+                        <Route path="/rentals" component={Rentals} />
+                        <Route path="/404" component={NotFound} />
+                        <Redirect from="/" exact to="/movies" />
+                        <Redirect to="/404" />
+                    </Switch>
+                </main>
+            </React.Fragment>
         );
     }
 }
