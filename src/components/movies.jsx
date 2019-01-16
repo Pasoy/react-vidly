a5697d415e375efdc2503f74b3f877cfc3e7939b
Pasoy <43441885+Pasoy@users.noreply.github.com>
2019-01-16 16:16:40 +0100

    Movies table, all genres...
    - Created MoviesTable component to have same level of abstraction
    - Added a feature to sort movies by "All Genres"
    - Fixed a bug which prevented the movies to be shown due to pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import MoviesTable from './moviesTable';
+
 import { getMovies } from '../services/fakeMovieService';
 import { getGenres } from '../services/fakeGenreService';
 
-import Like from './common/like';
 import Pagination from './common/pagination';
 import ListGroup from './common/listGroup';
 
@@ -17,9 +18,11 @@ class Movies extends Component {
     };
 
     componentDidMount() {
+        const genres = [{ name: 'All Genres' }, ...getGenres()];
+
         this.setState({
             movies: getMovies(),
-            genres: getGenres()
+            genres
         });
     }
 
@@ -41,7 +44,7 @@ class Movies extends Component {
     };
 
     handleGenreSelect = genre => {
-        this.setState({ selectedGenre: genre });
+        this.setState({ selectedGenre: genre, currentPage: 1 });
     };
 
     render() {
@@ -55,9 +58,10 @@ class Movies extends Component {
 
         if (count === 0) return <p>There are no movies in the database.</p>;
 
-        const filtered = selectedGenre
-            ? allMovies.filter(m => m.genre._id === selectedGenre._id)
-            : allMovies;
+        const filtered =
+            selectedGenre && selectedGenre._id
+                ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+                : allMovies;
         const movies = paginate(filtered, currentPage, pageSize);
 
         return (
@@ -71,46 +75,11 @@ class Movies extends Component {
                 </div>
                 <div className="col-md">
                     <p>Showing {filtered.length} movies in the database.</p>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Genre</th>
-                                <th>Stock</th>
-                                <th>Rate</th>
-                                <th />
-                                <th />
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {movies.map(movie => (
-                                <tr key={movie._id}>
-                                    <td>{movie.title}</td>
-                                    <td>{movie.genre.name}</td>
-                                    <td>{movie.numberInStock}</td>
-                                    <td>{movie.dailyRentalRate}</td>
-                                    <td>
-                                        <Like
-                                            liked={movie.liked}
-                                            onClick={() =>
-                                                this.handleLike(movie)
-                                            }
-                                        />
-                                    </td>
-                                    <td>
-                                        <button
-                                            onClick={() =>
-                                                this.handleDelete(movie)
-                                            }
-                                            className="btn btn-danger btn-sm"
-                                        >
-                                            Delete
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    <MoviesTable
+                        movies={movies}
+                        onLike={this.handleLike}
+                        onDelete={this.handleDelete}
+                    />
                     <Pagination
                         itemsCount={filtered.length}
                         pageSize={pageSize}
