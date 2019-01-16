26e5f4d0dd172183d5f3a123c31eb983e230e7e6
Pasoy <43441885+Pasoy@users.noreply.github.com>
2019-01-16 17:14:02 +0100

    Complete table component...
    - Created a complete table component which can be reused for later
    - Created methods to make the code "look cleaner"

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -53,8 +53,7 @@ class Movies extends Component {
         this.setState({ sortColumn });
     };
 
-    render() {
-        const { length: count } = this.state.movies;
+    getPagedData = () => {
         const {
             pageSize,
             currentPage,
@@ -62,8 +61,6 @@ class Movies extends Component {
             sortColumn,
             movies: allMovies
         } = this.state;
-
-        if (count === 0) return <p>There are no movies in the database.</p>;
 
         const filtered =
             selectedGenre && selectedGenre._id
@@ -78,6 +75,17 @@ class Movies extends Component {
 
         const movies = paginate(sorted, currentPage, pageSize);
 
+        return { totalCount: filtered.length, data: movies };
+    };
+
+    render() {
+        const { length: count } = this.state.movies;
+        const { pageSize, currentPage, sortColumn } = this.state;
+
+        if (count === 0) return <p>There are no movies in the database.</p>;
+
+        const { totalCount, data: movies } = this.getPagedData();
+
         return (
             <div className="row">
                 <div className="col-md-3">
@@ -88,7 +96,7 @@ class Movies extends Component {
                     />
                 </div>
                 <div className="col-md">
-                    <p>Showing {filtered.length} movies in the database.</p>
+                    <p>Showing {totalCount} movies in the database.</p>
                     <MoviesTable
                         movies={movies}
                         onLike={this.handleLike}
@@ -97,7 +105,7 @@ class Movies extends Component {
                         sortColumn={sortColumn}
                     />
                     <Pagination
-                        itemsCount={filtered.length}
+                        itemsCount={totalCount}
                         pageSize={pageSize}
                         currentPage={currentPage}
                         onPageChange={this.handlePageChange}
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 
 import Like from './common/like';
-import TableHeader from './common/tableHeader';
-import TableBody from './common/tableBody';
+import Table from './common/table';
 
 class MoviesTable extends Component {
     columns = [
@@ -36,14 +35,12 @@ class MoviesTable extends Component {
         const { movies, onSort, sortColumn } = this.props;
 
         return (
-            <table className="table">
-                <TableHeader
-                    columns={this.columns}
-                    sortColumn={sortColumn}
-                    onSort={onSort}
-                />
-                <TableBody columns={this.columns} data={movies} />
-            </table>
+            <Table
+                columns={this.columns}
+                data={movies}
+                sortColumn={sortColumn}
+                onSort={onSort}
+            />
         );
     }
 }
